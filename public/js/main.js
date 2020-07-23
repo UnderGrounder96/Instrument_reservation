e2c937a1175fa667802096aec314595d931a9c36
Lucio Afonso <lucioafonso@icloud.com>
2020-07-23 20:55:01 +0200

    Header updated NavBar responsiveness public/js/script.js removed

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,37 @@
 $.noConflict();
 
 jQuery(() => {
+  jQuery(".hide").hide();
+
+  jQuery(".topRight").addClass("nav-item nav-link text-body");
+
+  jQuery("#inst").click(() => {
     jQuery(".hide").hide();
+    jQuery("#inst1").show();
+  });
 
-    jQuery(".topRight").addClass("nav-item nav-link text-body");
+  jQuery("#user").click(() => {
+    jQuery(".hide").hide();
+    jQuery("#user1").show();
+  });
 
-    jQuery("#inst").click(function() {
-        jQuery(".hide").hide();
-        jQuery("#inst1").show();
-    });
-
-    jQuery("#user").click(function() {
-        jQuery(".hide").hide();
-        jQuery("#user1").show();
-    });
-
-    jQuery("#rig").click(function() {
-        jQuery(".hide").hide();
-        jQuery("#rig1").show();
-    });
+  jQuery("#rig").click(() => {
+    jQuery(".hide").hide();
+    jQuery("#rig1").show();
+  });
 });
+
+/*
+// NOT WORKING
+
+  <?php
+    if(isset($_GET["use"]) || isset($_GET["er"]))
+      echo "jQuery(\"#user1\").show();\n";
+
+    else if(isset($_GET["rig"]))
+      echo "jQuery(\"#rig1\").show()\n";
+
+    else
+      echo "jQuery(\"#inst1\").show();\n";
+  >
+*/
--- src/footer.php
+++ src/footer.php
@@ -1,4 +1,4 @@
-<footer class="page-footer align-bottom py-3 my-3 border-top">
+<footer class="page-footer py-3 my-3 border-top -light bg-light">
   <div class="footer-copyright pb-3 text-center">
    <?php echo '&copy; ' . date("Y") . ' <strong>Company</strong>. All rights reserved.';?>
   </div>
