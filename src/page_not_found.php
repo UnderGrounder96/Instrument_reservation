e29f60889fa987ee5f0c9f4bcf702f31d391f763
Lucio Afonso <lucioafonso@icloud.com>
2020-07-23 18:10:01 +0200

    Custom 404 error page added

---
+++ src/page_not_found.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+session_start();
+$title_page = "404 Error";
+require_once("head.php");
+?>
+
+<body>
+  <noscript> Please turn on JavaScript or change browsers!</noscript>
+
+  <?php
+  require_once("header.php");
+  ?>
+
+  <main class="container text-center" role="main">
+<pre class="lead">
+  <?php echo $_SERVER['REQUEST_URI']; ?> does not exist, sorry.
+</pre>
+
+    <img
+        class="img-fluid mx-auto d-block"
+        loading="lazy"
+        src="../public/img/error.png"
+        loading="lazy"
+        alt="not_found"
+      />
+  </main>
+
+  <?php
+  require_once("footer.php");
+  ?>
+
+</body>
+
+</html>
